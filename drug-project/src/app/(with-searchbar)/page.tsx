4e77dd966012c2f04e66e1341613629f42435bf8
uuizdashdw <chany920@gmail.com>
2025-09-09 18:33:59 +0900

    refactor : 페이지네이션 추가

--- drug-project/src/app/(with-searchbar)/page.tsx
+++ drug-project/src/app/(with-searchbar)/page.tsx
@@ -1,23 +1,35 @@
+// API
 import { getMedicineList } from '@/api/drugs';
-import DrugList from '@/components/common/DrugList';
-import { MedicineListParams } from '@/types/api';
 
-export default async function Home() {
+// Components
+import DrugList from '@/components/common/DrugList';
+import Pagination from '@/components/common/Pagination';
+
+// Types
+import { MedicineListParams } from '@/types/api';
+import { HomeProps } from '@/types/home';
+
+export default async function Home({ searchParams }: HomeProps) {
+    const pageNo = Number(searchParams?.page ?? '1');
+
     const params: MedicineListParams = {
         serviceKey: process.env?.DRUG_API_KEY ?? '',
         numOfRows: 12,
-        pageNo: 1,
+        pageNo: pageNo,
         Prduct: '',
         type: 'json',
     };
     const data = await getMedicineList(params);
-
     console.log('## 데이터 :: ', data);
 
-    // const data = await fetchDrugs('');
     return (
         <div>
             <DrugList drugs={data.body?.items} />
+            <Pagination
+                currentPage={pageNo}
+                totalCount={data.body?.totalCount ?? 0}
+                pageSize={12}
+            />
         </div>
     );
 }
