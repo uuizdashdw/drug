e4573ab0b54af842b3fd586abb91c68f2cd96eab
uuizdashdw <chany920@gmail.com>
2025-09-09 16:01:58 +0900

    refactor : 서치바 디자인 수정

--- drug-project/src/components/search/SearchBar.tsx
+++ drug-project/src/components/search/SearchBar.tsx
@@ -20,9 +20,10 @@ export default function SearchBar() {
     }, [searchValue]);
 
     return (
-        <div>
+        <div className="mx-auto mb-14 flex w-8/12 items-center justify-center gap-10">
             <input
                 type="text"
+                name="search"
                 placeholder="찾고자 하는 의약품을 검색해주세요!"
                 value={searchValue}
                 onChange={onChangeSearchValue}
@@ -30,8 +31,14 @@ export default function SearchBar() {
                     if (e.key !== 'Enter') return;
                     handleOnSearch();
                 }}
+                className="border-brand-300 w-6/12 rounded-sm border-2 outline-0 focus:ring-0 focus:outline-none"
             />
-            <button onClick={handleOnSearch}>검색</button>
+            <button
+                className="bg-brand-300 rounded-sm px-5 py-2.5 font-bold text-white"
+                onClick={handleOnSearch}
+            >
+                검색
+            </button>
         </div>
     );
 }
