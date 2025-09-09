410d2606f9d749ff710534070aaee506ad8aec48
uuizdashdw <chany920@gmail.com>
2025-09-09 19:17:10 +0900

    refactor : 약물 아이템 구조 변경

--- drug-project/src/components/common/DrugItem.tsx
+++ drug-project/src/components/common/DrugItem.tsx
@@ -2,14 +2,24 @@
 
 import { DrugItemProps } from '@/types/drug';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect } from 'react';
 
 export default function DrugItem({ drug }: DrugItemProps) {
+    useEffect(() => {
+        console.log('## 약물 상세 :: ', drug);
+    }, [drug]);
     return (
-        <div>
-            <div>
-                <Image src={drug?.ITEM_IMAGE} alt={drug?.ITEM_IMAGE} width={300} height={500} />
-                <p>{drug?.ITEM_NAME}</p>
-            </div>
-        </div>
+        <Link href={`/drug/${drug?.ITEM_SEQ}`} className="flex flex-col items-center">
+            <Image
+                src={drug?.ITEM_IMAGE}
+                alt={drug?.ITEM_IMAGE}
+                width={300}
+                height={500}
+                className="mb-3"
+                loading="lazy"
+            />
+            <p className="text-sm text-stone-950">{drug?.ITEM_NAME}</p>
+        </Link>
     );
 }
