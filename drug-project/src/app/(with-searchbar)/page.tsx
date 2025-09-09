d1dfc667d6c7dfd12607de4a7a7899390a68707b
uuizdashdw <chany920@gmail.com>
2025-09-09 12:59:54 +0900

    add : 메인 레이아웃 / 페이지 컴포넌트 및 파비콘, 글로벌 css 파일 위치 변경 (라우트 그룹화)

---
+++ drug-project/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <main className="">
+                <p className="text-brand-500 text-3xl">테일윈드 에러 </p>
+            </main>
+        </div>
+    );
+}
