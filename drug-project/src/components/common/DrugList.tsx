498154ac54bcc5beb9aa87edd3008b1da70864e1
uuizdashdw <chany920@gmail.com>
2025-09-09 19:16:40 +0900

    refactor : 약물 리스트 구조 변경 및 스타일 변경

--- drug-project/src/components/common/DrugList.tsx
+++ drug-project/src/components/common/DrugList.tsx
@@ -1,18 +1,16 @@
 'use client';
 
 import { DrugListProps } from '@/types/drug';
-import { useEffect } from 'react';
 import DrugItem from './DrugItem';
 
 export default function DrugList({ drugs }: DrugListProps) {
-    useEffect(() => {
-        console.log('## 약물 :: ', drugs);
-    }, [drugs]);
     return (
-        <div className="grid grid-cols-3 gap-32">
-            {drugs?.map((item, idx) => (
-                <DrugItem drug={item} key={idx} />
-            ))}
+        <div className="mb-32">
+            <div className="grid grid-cols-3 gap-32">
+                {drugs?.map((item, idx) => (
+                    <DrugItem drug={item} key={idx} />
+                ))}
+            </div>
         </div>
     );
 }
