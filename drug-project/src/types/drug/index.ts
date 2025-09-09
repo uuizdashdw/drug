ed69674d806740218c50587fa922aa720f0e7b06
uuizdashdw <chany920@gmail.com>
2025-09-09 17:51:09 +0900

    add : 약물 타입 추가

---
+++ drug-project/src/types/drug/index.ts
@@ -0,0 +1,43 @@
+export interface DrugItem {
+    ITEM_SEQ: string;
+    ITEM_NAME: string;
+    ENTP_SEQ: string;
+    ENTP_NAME: string;
+    CHART: string;
+    ITEM_IMAGE: string;
+    PRINT_FRONT: string;
+    PRINT_BACK: string;
+    DRUG_SHAPE: string;
+    COLOR_CLASS1: string;
+    COLOR_CLASS2: string;
+    LINE_FRONT: string;
+    LINE_BACK: string;
+    LENG_LONG: string;
+    LENG_SHORT: string;
+    THICK: string;
+    IMG_REGIST_TS: string;
+    CLASS_NO: string;
+    CLASS_NAME: string;
+    ETC_OTC_NAME: string;
+    ITEM_PERMIT_DATE: string;
+    FORM_CODE_NAME: string;
+    MARK_CODE_FRONT_ANAL: string;
+    MARK_CODE_BACK_ANAL: string;
+    MARK_CODE_FRONT_IMG: string;
+    MARK_CODE_BACK_IMG: string;
+    ITEM_ENG_NAME: string;
+    CHANGE_DATE: string;
+    MARK_CODE_FRONT: string;
+    MARK_CODE_BACK: string;
+    EDI_CODE: string;
+    BIZRNO: string;
+    STD_CD: string;
+}
+
+export interface DrugListProps {
+    drugs: DrugItem[];
+}
+
+export interface DrugItemProps {
+    drug: DrugItem;
+}
