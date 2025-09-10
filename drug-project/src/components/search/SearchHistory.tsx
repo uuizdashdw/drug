3414a96f17cb2ac196537647dfdccf3e4c62fc42
uuizdashdw <chany920@gmail.com>
2025-09-10 18:53:39 +0900

    refactor: 최근 검색어 목록 스타일 수정

--- drug-project/src/components/search/SearchHistory.tsx
+++ drug-project/src/components/search/SearchHistory.tsx
@@ -7,7 +7,7 @@ export default function SearchHistroy() {
     const { queries, addQuery } = useSearchStore();
     return (
         <div className="mx-auto mb-14 w-5/12">
-            <ul className="flex gap-4">
+            <ul className="flex flex-wrap items-center gap-4 px-4">
                 {queries?.map((keyword, index) => (
                     <li key={index} className="border-brand-100 rounded-md border px-3.5 py-1">
                         <Link href={`/search?q=${encodeURIComponent(keyword)}`}>
