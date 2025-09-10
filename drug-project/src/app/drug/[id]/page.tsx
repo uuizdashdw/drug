06ba0eedb42263fc7698cc122e31012802de8ddf
uuizdashdw <chany920@gmail.com>
2025-09-10 13:35:18 +0900

    refactor : 변경 / 등록 이력 컴포넌트 적용

--- drug-project/src/app/drug/[id]/page.tsx
+++ drug-project/src/app/drug/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { getMedicineList } from '@/api/drugs';
 import BasicInfo from '@/components/drug/detail/BasicInfo';
+import ChangeOrRegistrationHistory from '@/components/drug/detail/ChangeOrRegistrationHistory';
 import ExteriorInfo from '@/components/drug/detail/ExteriorInfo';
 import ManufacturerInfo from '@/components/drug/detail/ManufacturerInfo';
 import MarkAndCodeInfo from '@/components/drug/detail/MarkAndCodeInfo';
 import {
     BaiscInfoProps,
+    ChangeOrRegistrationHistoryProps,
     DrugDetailItemProps,
     DrugItem,
     ExteriorInfoProps,
@@ -67,6 +69,11 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
         markCodeBackImage: drug?.MARK_CODE_BACK_IMG,
     } as MarkAndCodeInfoProps;
 
+    const changeOrRegistrationHistoryProps = {
+        imageRegistTs: drug?.IMG_REGIST_TS,
+        changeDate: drug?.CHANGE_DATE,
+    } as ChangeOrRegistrationHistoryProps;
+
     return (
         <div className="p-4">
             <h1 className="mb-16 text-center text-3xl font-bold">{drug.ITEM_NAME}</h1>
@@ -100,55 +107,11 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
 
                 {/* 마크 / 코드 정보 */}
                 <MarkAndCodeInfo {...markCodeInfoProps} />
-                {/* <section className="mb-6">
-                    <h2 className="mb-3 text-2xl font-bold">마크 / 코드</h2>
-
-                    <p className="mb-1">
-                        <strong>앞면 마크 : </strong> {drug?.MARK_CODE_FRONT ?? '-'}
-                    </p>
-                    <p>
-                        <strong>뒷면 마크 : </strong> {drug?.MARK_CODE_BACK ?? '-'}
-                    </p>
-                    <p className="mb-1">
-                        <strong>EDI 코드 : </strong> {drug?.EDI_CODE ?? '-'}
-                    </p>
-                    <p className="mb-1">
-                        <strong>표준코드 : </strong> {drug.STD_CD ?? '-'}
-                    </p>
-
-                    <div className="mt-3 flex gap-4">
-                        {drug.MARK_CODE_FRONT_IMG && (
-                            <Image
-                                src={drug.MARK_CODE_FRONT_IMG}
-                                alt="앞면 마크 이미지"
-                                className="h-20 w-20 rounded border object-contain"
-                                width={100}
-                                height={100}
-                            />
-                        )}
-                        {drug.MARK_CODE_BACK_IMG && (
-                            <Image
-                                src={drug.MARK_CODE_BACK_IMG}
-                                alt="뒷면 마크 이미지"
-                                className="h-20 w-20 rounded border object-contain"
-                            />
-                        )}
-                    </div>
-                </section> */}
 
                 <hr className="my-24 border-t border-gray-300" />
 
-                <section className="mb-6">
-                    <h2 className="mb-3 text-2xl font-bold">변경 / 등록 이력</h2>
-
-                    <p className="mb-1">
-                        <strong>이미지 등록일자 : </strong>{' '}
-                        {tranformDateYYYYMMDD(drug?.IMG_REGIST_TS)}
-                    </p>
-                    <p className="mb-1">
-                        <strong>변경일자 : </strong> {tranformDateYYYYMMDD(drug?.CHANGE_DATE)}
-                    </p>
-                </section>
+                {/* 변경 / 등록 이력 */}
+                <ChangeOrRegistrationHistory {...changeOrRegistrationHistoryProps} />
             </div>
         </div>
     );
