20335d038bf021347e5f75f2e116a307b14a21ea
uuizdashdw <chany920@gmail.com>
2025-09-10 13:04:50 +0900

    add : 제조사 정보 컴포넌트 생성

---
+++ drug-project/src/components/drug/detail/ManufacturerInfo.tsx
@@ -0,0 +1,20 @@
+import { ManufacturerInfoProps } from '@/types/drug';
+
+export default function ManufacturerInfo({ entpName, bizrNo, entpSeq }: ManufacturerInfoProps) {
+    return (
+        <section className="mb-6">
+            <h2 className="mb-2 text-2xl font-bold">제조사 정보</h2>
+            <p className="mb-1">
+                <strong>업체명 : </strong>
+                {entpName}
+            </p>
+            <p className="mb-1">
+                <strong>사업자등록번호 : </strong> {bizrNo}
+            </p>
+            <p className="mb-1">
+                <strong>업체 코드 : </strong>
+                {entpSeq}
+            </p>
+        </section>
+    );
+}
