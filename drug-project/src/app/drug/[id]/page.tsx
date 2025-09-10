9704ef4085c11d1ee4e985fcc3b67204373fe2a9
uuizdashdw <chany920@gmail.com>
2025-09-10 13:00:31 +0900

    refactor : 약물 기본정보 컴포넌트 적용

--- drug-project/src/app/drug/[id]/page.tsx
+++ drug-project/src/app/drug/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getMedicineList } from '@/api/drugs';
+import BasicInfo from '@/components/drug/detail/BasicInfo';
 import { DrugDetailItemProps, DrugItem } from '@/types/drug';
 import { tranformDateYYYYMMDD } from '@/utils/transformDate';
 import Image from 'next/image';
@@ -38,36 +39,25 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
 
             <div className="mx-auto w-2xl">
                 {/* 기본 정보 */}
-                <section className="mb-6">
-                    <h2 className="mb-2 text-2xl font-bold">기본 정보</h2>
-                    <p>
-                        <strong>제품명 :</strong> {drug?.ITEM_NAME}
-                    </p>
-                    <p>
-                        <strong>약효 분류 :</strong> {drug?.CLASS_NAME}
-                    </p>
-                    <p>
-                        <strong>약효 고유 번호 :</strong> {drug?.CLASS_NO}
-                    </p>
-                    <p>
-                        <strong>전문/일반 구분 :</strong> {drug?.ETC_OTC_NAME}
-                    </p>
-                    <p>
-                        <strong>허가일자 :</strong> {tranformDateYYYYMMDD(drug?.ITEM_PERMIT_DATE)}
-                    </p>
-                </section>
+                <BasicInfo
+                    itemName={drug?.ITEM_NAME}
+                    className={drug?.CLASS_NAME}
+                    classNo={drug?.CLASS_NO}
+                    etcOtcName={drug?.ETC_OTC_NAME}
+                    itemPermitDate={drug?.ITEM_PERMIT_DATE}
+                />
 
                 <hr className="my-24 border-t border-gray-300" />
 
                 <section className="mb-6">
                     <h2 className="mb-2 text-2xl font-bold">제조사 정보</h2>
-                    <p>
+                    <p className="mb-1">
                         <strong>업체명 :</strong> {drug.ENTP_NAME}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>사업자등록번호 :</strong> {drug.BIZRNO}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>업체 코드 :</strong> {drug.ENTP_SEQ}
                     </p>
                 </section>
@@ -88,20 +78,20 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
                         <p className="mt-2 text-sm text-gray-500">{drug.CHART}</p>
                     </div>
 
-                    <p>
+                    <p className="mb-1">
                         <strong>모양 :</strong> {drug.DRUG_SHAPE}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>색상 :</strong> {drug.COLOR_CLASS1} {drug.COLOR_CLASS2 ?? ''}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>각인 :</strong> 앞면 {drug.PRINT_FRONT} / 뒷면 {drug.PRINT_BACK}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>분할선 :</strong> 앞면 {drug.PRINT_FRONT ?? '-'} / 뒷면{' '}
                         {drug.PRINT_BACK ?? '-'}
                     </p>
-                    <p>
+                    <p className="mb-1">
                         <strong>크기 :</strong> 길이 {drug.LENG_LONG}mm × {drug.LENG_SHORT}mm, 두께{' '}
                         {drug.THICK}mm
                     </p>
@@ -112,20 +102,18 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
                 <section className="mb-6">
                     <h2 className="mb-3 text-2xl font-bold">마크 / 코드</h2>
 
-                    <div>
-                        <p>
-                            <strong>앞면 마크 : </strong> {drug?.MARK_CODE_FRONT ?? '-'}
-                        </p>
-                        <p>
-                            <strong>뒷면 마크 : </strong> {drug?.MARK_CODE_BACK ?? '-'}
-                        </p>
-                        <p>
-                            <strong>EDI 코드 : </strong> {drug?.EDI_CODE ?? '-'}
-                        </p>
-                        <p>
-                            <strong>표준코드 : </strong> {drug.STD_CD ?? '-'}
-                        </p>
-                    </div>
+                    <p className="mb-1">
+                        <strong>앞면 마크 : </strong> {drug?.MARK_CODE_FRONT ?? '-'}
+                    </p>
+                    <p>
+                        <strong>뒷면 마크 : </strong> {drug?.MARK_CODE_BACK ?? '-'}
+                    </p>
+                    <p className="mb-1">
+                        <strong>EDI 코드 : </strong> {drug?.EDI_CODE ?? '-'}
+                    </p>
+                    <p className="mb-1">
+                        <strong>표준코드 : </strong> {drug.STD_CD ?? '-'}
+                    </p>
 
                     <div className="mt-3 flex gap-4">
                         {drug.MARK_CODE_FRONT_IMG && (
@@ -145,6 +133,20 @@ export default async function DrugDetailItem({ params }: DrugDetailItemProps) {
                             />
                         )}
                     </div>
+                </section>
+
+                <hr className="my-24 border-t border-gray-300" />
+
+                <section className="mb-6">
+                    <h2 className="mb-3 text-2xl font-bold">변경 / 등록 이력</h2>
+
+                    <p className="mb-1">
+                        <strong>이미지 등록일자 : </strong>{' '}
+                        {tranformDateYYYYMMDD(drug?.IMG_REGIST_TS)}
+                    </p>
+                    <p className="mb-1">
+                        <strong>변경일자 : </strong> {tranformDateYYYYMMDD(drug?.CHANGE_DATE)}
+                    </p>
                 </section>
             </div>
         </div>
