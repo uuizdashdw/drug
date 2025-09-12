caa2eb60ed498a91186afa1226f7ebe821372c70
uuizdashdw <chany920@gmail.com>
2025-09-12 16:37:53 +0900

    add : 약국 검색 페이지 추가

---
+++ drug-project/src/app/(with-searchbar)/search/pharmacy/page.tsx
@@ -0,0 +1,50 @@
+import { getPharmacyList } from '@/api/pharmacy';
+import Pagination from '@/components/common/Pagination';
+import PharmacyList from '@/components/pharmacy/PharmacyList';
+import NoContent from '@/components/search/NoContent';
+import SearchHistroy from '@/components/search/SearchHistory';
+import SearchResultGuide from '@/components/search/SearchResultGuide';
+import { SearchPharmacyPageProps } from '@/types/search';
+
+export default async function PharmacySearchPage({ searchParams }: SearchPharmacyPageProps) {
+    const param = await searchParams;
+    const keyword = param?.q;
+    const pageNo = param?.pageNo;
+    console.log('## 파라미터 :: ', param);
+
+    const data = await getPharmacyList({
+        serviceKey: process.env.SERVICE_API_KEY ?? '',
+        yadmNm: keyword,
+        pageNo: Number(pageNo),
+        numOfRows: 12,
+    });
+
+    // console.log(' 약국 검색 결과 :: ', data?.response?.body);
+    return (
+        <div className="container">
+            <SearchHistroy />
+
+            {Array.isArray(data?.response?.body?.items?.item) &&
+                data?.response?.body?.items?.item?.length > 0 && (
+                    <>
+                        {keyword && (
+                            <SearchResultGuide
+                                itemName={keyword}
+                                length={data?.response?.body?.items?.item?.length}
+                            />
+                        )}
+                        <PharmacyList pharmacies={data.response?.body?.items?.item || []} />
+                    </>
+                )}
+
+            {(!Array.isArray(data?.response?.body?.items?.item) ||
+                data?.response?.body?.items?.item?.length === 0) && <NoContent keyword={keyword} />}
+
+            <Pagination
+                currentPage={Number(pageNo)}
+                totalCount={data?.response?.body?.totalCount ?? 0}
+                pageSize={12}
+            />
+        </div>
+    );
+}
