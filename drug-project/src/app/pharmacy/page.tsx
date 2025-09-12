9cc1498778e77ae25cfaa9a7d109e3ada206f5d2
uuizdashdw <chany920@gmail.com>
2025-09-12 15:20:43 +0900

    add : 약국 찾기 페이지 생성

---
+++ drug-project/src/app/pharmacy/page.tsx
@@ -0,0 +1,30 @@
+import { getPharmacyList } from '@/api/pharmacy';
+import Pagination from '@/components/common/Pagination';
+import PharmacyList from '@/components/pharmacy/PharmacyList';
+import SearchHistroy from '@/components/search/SearchHistory';
+import { PharmacyPageProps } from '@/types/pharmacy';
+
+export default async function PharmacyPage({ searchParams }: PharmacyPageProps) {
+    const param = searchParams;
+    const pageNo = Number(param?.page ?? '1');
+
+    const data = await getPharmacyList({
+        serviceKey: process.env.SERVICE_API_KEY ?? '',
+        pageNo: pageNo,
+        numOfRows: 12,
+    });
+
+    console.log('## 약국 :: ', data?.response);
+    return (
+        <div>
+            <SearchHistroy />
+
+            <PharmacyList pharmacies={data?.response?.body?.items?.item} />
+            <Pagination
+                currentPage={pageNo}
+                totalCount={data?.response?.body?.totalCount ?? 0}
+                pageSize={12}
+            />
+        </div>
+    );
+}
