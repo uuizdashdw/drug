6034fb4e6899e06778d713da58855c7b8eaf5ad7
uuizdashdw <chany920@gmail.com>
2025-09-12 16:40:22 +0900

    fix : 공공 API 변경으로 인한 컴포넌트 수정

--- drug-project/src/app/pharmacy/[id]/page.tsx
+++ drug-project/src/app/pharmacy/[id]/page.tsx
@@ -1,6 +1,4 @@
-import { getPharmacyList } from '@/api/pharmacy';
 import ImageAndItem from '@/components/drug/detail/ImageAndItem';
-import BusinessHour from '@/components/pharmacy/detail/BusinessHour';
 import PharmBaiscInfo from '@/components/pharmacy/detail/PharmBasicInfo';
 import { ImageAndItemProps } from '@/types/drug';
 import { DetailPharmacyItemProps, PharmacyItem, PharmBaiscInfoProps } from '@/types/pharmacy';
@@ -9,18 +7,15 @@ export default async function DetailPharmacyItem({ params }: DetailPharmacyItemP
     const { id } = await params;
     const detailPharmacyItem: PharmacyItem = JSON.parse(decodeURIComponent(id));
 
-    console.log(' 약국 상세 정보 :: ', detailPharmacyItem);
-
     const imageAndItemProps = {
-        itemName: detailPharmacyItem?.dutyName,
+        itemName: detailPharmacyItem?.yadmNm,
         itemImage: null,
     } as ImageAndItemProps;
 
     const pharmBaiscInfo = {
-        address: detailPharmacyItem?.dutyAddr,
-        postNumber: detailPharmacyItem?.postCdn1 + '-' + detailPharmacyItem?.postCdn2,
-        telNumber: detailPharmacyItem?.dutyTel1,
-        etc: detailPharmacyItem?.dutyEtc,
+        address: detailPharmacyItem?.addr,
+        postNumber: String(detailPharmacyItem?.postNo),
+        telNumber: detailPharmacyItem?.telno,
     } as PharmBaiscInfoProps;
 
     return (
@@ -30,9 +25,7 @@ export default async function DetailPharmacyItem({ params }: DetailPharmacyItemP
             <div className="mx-auto w-2xl">
                 <PharmBaiscInfo {...pharmBaiscInfo} />
 
-                <hr className="my-24 border-t border-gray-300" />
-
-                <BusinessHour pharmacy={detailPharmacyItem} />
+                {/* <hr className="my-24 border-t border-gray-300" /> */}
             </div>
         </div>
     );
