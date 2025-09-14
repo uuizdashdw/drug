c000f55413ca8e317b439b345762369659764bd5
uuizdashdw <chany920@gmail.com>
2025-09-14 15:00:22 +0900

    add : jest.config.ts 로 확장자명 변경 및 Jest 설정 파일 추가

---
+++ drug-project/jest.config.ts
@@ -0,0 +1,13 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(customJestConfig);
