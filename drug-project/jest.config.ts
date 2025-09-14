9e55c4aadef8e7511f655caca23abdf4cb0088df
uuizdashdw <chany920@gmail.com>
2025-09-14 15:36:54 +0900

    refactor : Jest가 tsconfig alias를 자동으로 읽지 못할 경우를 대비해 pathsToModuleNameMapper 를 사용하기 위한 ts-jest 설치 및 적용

--- drug-project/jest.config.ts
+++ drug-project/jest.config.ts
@@ -1,13 +1,17 @@
 import nextJest from 'next/jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+        ...pathsToModuleNameMapper(compilerOptions.paths, {
+            prefix: '<rootDir>/',
+        }),
+    },
 };
 
 export default createJestConfig(customJestConfig);
