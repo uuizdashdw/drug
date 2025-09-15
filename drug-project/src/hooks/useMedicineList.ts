ab3351c148c132cc8d527bda9b650319fcdd51fb
uuizdashdw <chany920@gmail.com>
2025-09-15 20:04:44 +0900

    fix : api 수정으로 인한 훅 수정

--- drug-project/src/hooks/useMedicineList.ts
+++ drug-project/src/hooks/useMedicineList.ts
@@ -1,4 +1,5 @@
 import { getMedicineList } from '@/api/drugs';
+import { useErrorModalStore } from '@/store/zustand/common/errorModalState';
 import { useQuery } from '@tanstack/react-query';
 import { keepPreviousData } from '@tanstack/react-query';
 import { useEffect } from 'react';
@@ -6,18 +7,27 @@ import { useEffect } from 'react';
 type MedicineResponse = Awaited<ReturnType<typeof getMedicineList>>;
 
 export const useMedicineList = (pageNo: number) => {
+    const { open } = useErrorModalStore();
     const query = useQuery({
         queryKey: ['medicineList', pageNo],
-        queryFn: () =>
-            getMedicineList({
-                serviceKey: process.env.NEXT_PUBLIC_SERVICE_API_KEY ?? '',
-                itemName: '',
-                numOfRows: 20,
-                pageNo,
-                type: 'json',
-            }),
-        placeholderData: keepPreviousData,
-        staleTime: 1000 * 60, // 1분 동안은 fresh 상태로 유지
+        queryFn: async () => {
+            try {
+                return await getMedicineList({
+                    serviceKey:
+                        process.env.SERVICE_API_KEY ??
+                        '582eb8992a09b966d969483025088ab6e10b06829cb8fce85f9edfd58c785a99',
+                    itemName: '',
+                    numOfRows: 20,
+                    pageNo,
+                    type: 'json',
+                });
+            } catch (e) {
+                console.error('❌ API 실패', e);
+                return { body: { items: [], totalCount: 0 } }; // 최소 안전 리턴
+            }
+        },
+        // placeholderData: keepPreviousData,
+        // staleTime: 1000 * 60, // 1분 동안은 fresh 상태로 유지
     });
 
     useEffect(() => {
@@ -27,6 +37,8 @@ export const useMedicineList = (pageNo: number) => {
         // openErrorModal(msg);
         // 또는 콜백을 훅 파라미터로 받아서:
         // onErrorModal?.(msg);
+
+        open(msg);
         console.error('❌ 약 리스트 불러오기 실패:', query.error);
     }, [query.isError, query.error]);
 
