cf3afeb8cc0e6dd01f3955eeb61127915752230b
uuizdashdw <chany920@gmail.com>
2025-09-15 21:30:13 +0900

    refactor : 리액트 쿼리 적용한 훅 적용 및 서버 컴포넌트와 클라이언트 컴포넌트 분리 (구조 재조정)

--- drug-project/src/app/pharmacy/page.tsx
+++ drug-project/src/app/pharmacy/page.tsx
@@ -1,37 +1,19 @@
-import { getPharmacyList } from '@/api/pharmacy';
-import Pagination from '@/components/common/Pagination';
-import PharmacyList from '@/components/pharmacy/PharmacyList';
-import NoContent from '@/components/search/NoContent';
+// Components
+import PharmacyListPage from '@/components/pharmacy/PharmacyListPage';
 import SearchHistroy from '@/components/search/SearchHistory';
+
+// Types
 import { PharmacyPageProps } from '@/types/pharmacy';
 
 export default async function PharmacyPage({ searchParams }: PharmacyPageProps) {
     const param = await searchParams;
     const pageNo = Number(param?.page ?? '1');
 
-    const data = await getPharmacyList({
-        serviceKey: process.env.SERVICE_API_KEY ?? '',
-        pageNo: pageNo,
-        numOfRows: 20,
-    });
-
     return (
-        <div>
+        <>
             <SearchHistroy />
 
-            {Array.isArray(data.body?.items) && data.body?.items?.length > 0 && (
-                <>
-                    <PharmacyList pharmacies={data?.response?.body?.items?.item} />
-                    <Pagination
-                        currentPage={pageNo}
-                        totalCount={data?.response?.body?.totalCount ?? 0}
-                        pageSize={12}
-                    />
-                </>
-            )}
-
-            {!Array.isArray(data.body?.items) ||
-                (data.body?.items?.length === 0 && <NoContent keyword={''} />)}
-        </div>
+            <PharmacyListPage pageNo={pageNo} />
+        </>
     );
 }
