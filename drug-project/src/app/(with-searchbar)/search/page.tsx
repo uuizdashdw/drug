bff1659631976938b825a455515b4e551bbe25a9
uuizdashdw <chany920@gmail.com>
2025-09-15 21:04:10 +0900

    refactor : 클라이언트 컴포넌트 적용

--- drug-project/src/app/(with-searchbar)/search/page.tsx
+++ drug-project/src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,8 @@
-import { getMedicineList } from '@/api/drugs';
-import DrugList from '@/components/common/DrugList';
-import Pagination from '@/components/common/Pagination';
-import NoContent from '@/components/search/NoContent';
+// Components
+import SearchDrugList from '@/components/search/SearchDrugList';
 import SearchHistroy from '@/components/search/SearchHistory';
-import SearchResultGuide from '@/components/search/SearchResultGuide';
 
+// Types
 import { SearchPageProps } from '@/types/search';
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
@@ -12,38 +10,11 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
     const itemName = param?.q ?? '';
     const pageNo = Number(param?.page ?? '1');
 
-    const data = await getMedicineList({
-        serviceKey: process.env.SERVICE_API_KEY ?? '',
-        itemName: itemName,
-        numOfRows: 20,
-        pageNo: pageNo,
-        type: 'json',
-    });
-
     return (
         <div className="container">
             <SearchHistroy />
 
-            {Array.isArray(data.body?.items) && data.body?.items?.length > 0 && (
-                <>
-                    {itemName && (
-                        <SearchResultGuide itemName={itemName} length={data.body?.items?.length} />
-                    )}
-                    <DrugList drugs={data.body?.items} />
-                </>
-            )}
-
-            {(!Array.isArray(data.body?.items) || data?.body?.items?.length === 0) && (
-                <NoContent keyword={itemName} />
-            )}
-
-            {Array.isArray(data.body?.items) && data.body?.items?.length > 0 && (
-                <Pagination
-                    currentPage={pageNo}
-                    totalCount={data.body?.totalCount ?? 0}
-                    pageSize={12}
-                />
-            )}
+            <SearchDrugList itemName={itemName} pageNo={pageNo} />
         </div>
     );
 }
