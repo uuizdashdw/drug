260ffc4255ca1147624a8a6005628a5415731b0a
uuizdashdw <chany920@gmail.com>
2025-09-18 17:44:55 +0900

    fix : 테스트 코드 수정

--- drug-project/src/__test__/app/components/search/SearchResultGuide.test.tsx
+++ drug-project/src/__test__/app/components/search/SearchResultGuide.test.tsx
@@ -3,7 +3,7 @@ import SearchResultGuide from '@/components/search/SearchResultGuide';
 
 describe('SearchResultGuide', () => {
     it('itemName과 length를 올바르게 출력한다', () => {
-        render(<SearchResultGuide itemName="타이레놀" length={5} />);
+        render(<SearchResultGuide itemName="타이레놀" totalCount={5} />);
 
         // 조각별 검증 (태그 분리 문제 해결)
         expect(screen.getByText('타이레놀')).toBeInTheDocument();
